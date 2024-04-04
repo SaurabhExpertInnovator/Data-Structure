cc02980478dbdc1b3181320f6c9fbbc0b32ca944
Saurabh Prajapati <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-04 23:24:54 +0530

    Binary.c

--- Binary.c
+++ Binary.c
@@ -10,13 +10,14 @@ int mid =(start+end)/2;
     return mid;
   }
   else if(arr[mid]<key){
-    mid=start+1;
+    start=mid+1;
   }
   else{
-    mid=end-1;
+    end=mid-1;
   }
-return -1;
 }
+return -1;
+
 }
 int main(){
   int arr[]={1,2,3,4,5,6,7,8,9};
